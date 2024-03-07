8603459a2da4cf38e53fd0f9163db0236f947a0c
RafaelCGConrado <106274045+RafaelCGConrado@users.noreply.github.com>
2024-03-06 22:13:51 -0300

    main e teste de conexao

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st 
+
+st.set_page_config(
+    layout = 'wide',
+    page_title = 'Projeto IC',
+    initial_sidebar_state="auto"
+)
+
+with st.sidebar:
+    st.write(
+        """
+        #Projeto de Iniciação Científica\n
+        Ainda não sei o nome dessa coisa!
+        """
+    )
+
